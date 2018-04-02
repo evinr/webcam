6ed3b0753c376818c1aca4116ab08b096c4676f0
Evin Revello <evin@evinrevello.com>
2018-04-02 11:50:50 -0700

    Finally making the scripted crons works

--- install.sh
+++ install.sh
@@ -6,19 +6,20 @@ if [ "${#IMAGE_ENDPOINT}" -lt "20" ]; then
 	echo -e "\n🙅 Your IMAGE_ENDPOINT environmental variable is not set 🌎\n" && 
 	echo "Please enter then end point of your image capturing funtion: " &&
 	read endpoint
-	echo "export IMAGE_ENDPOINT=$endpoint" >> ~/.bashrc &&
+	# verify that a POST request can be made via the same function that is making the POST
+	# TODO
+	# verify that the ~/.bash* is available what is being used on the system
+	# TODO
+	# adds what was entered on the command line to the bash config file
+	echo $endpoint >> ~/.bashrc
 	echo " please run 'source ~/.bashrc' to update this shell" &&
-	exit 1
 fi
 
 # Setup the crontabs
-	# tests if any crontabs exist
-export EDITOR="tee"
-	printf "*/1 * * * * ~/webcam/image-capture-n-upload.sh" | crontab -e
-unset EDITOR
-
+	(crontab -l 2>/dev/null; echo "*/1 * * * * ~/webcam/image-capture-n-upload.sh") | crontab -
 
 # Setup daily reboot to prevent unexpected behavior
-export EDITOR="tee"
-printf "@daily /sbin/shutdown -r now \n$(crontab -l root)\n" | sudo crontab -u root -e
-unset EDITOR
+	# printf "@daily /sbin/shutdown -r now \n$(crontab -l root)\n" | sudo crontab -u root -e
+	# test this with root
+	# TODO
+	(crontab -u root -l 2>/dev/null; echo "*/5 * * * * /path/to/job -with args") | crontab -
